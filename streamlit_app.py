25d13d2d20b6e2337d23102945e7ded67da8f0ad
Debbie Matthews <debbie.matthews@snowflake.com>
2024-10-31 19:48:39 -0700

    Add :streamlit: emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import pandas as pd
 
-@st.cache(ttl=60*60*12, allow_output_mutation=True)
+@st.cache_data(ttl=60*60*12)
 def fetch_emojis():
     resp = requests.get(
         'https://raw.githubusercontent.com/omnidan/node-emoji/master/lib/emoji.json')
@@ -24,6 +24,8 @@ Shortcodes are a way to enter emojis using pure ASCII. So you can type this `:sm
 (Keep in mind you can also enter emojis directly as Unicode in your Python strings too — you don't
 *have to* use a shortcode)
 '''
+
+st.info("In addition to the standard emojis listed below, add a little Streamlit flair with `:streamlit:` -> :streamlit:")
 
 emojis = fetch_emojis()
 
